e447ec11611398197d0bce6ce61a937d1da7efea
saywandeusher <ongsaywan@hotmail.com>
2018-06-01 23:01:51 +0800

    Completed assignment

--- script.js
+++ script.js
@@ -1,91 +1,5 @@
 //Q1
-//option 1
-/*
-var productsItems = products.items;
-var count = 0;
-productItems.forEach(function(item){
-  if (item.kind == "shopping#product")
-    count++;
-});
 
-console.log(count);
-
-//Q1
-//option 2
-var productsItems = products.items;
-function kindShoppingProduct(x) {
-  var kSP = 0;
-  for (var i = 0; i < x.length; i++){
-    if (x[i].kind == "shopping#product"){
-      kSP++;
-    }
-  }
-    return kSP;
-}
-console.log(kindShoppingProduct(productsItems));
-
-//Also found in "itemPerPage" and "currentItemCount"
-
-//Q2
-var productsItems = products.items;
-function availabilitybackorder(x) {
-  for (var i = 0; i < x.length; i++){
-    if (x[i].product.inventories[0].availability == "backorder"){
-      console.log(x[i].product.title);
-    }
-  }
-}
-console.log(availabilitybackorder(productsItems));
-
-
-//Q3
-var productsItems = products.items;
-function moreThanOneImage(x) {
-  for (var i = 0; i < x.length; i++){
-    if (x[i].product.images.length > 1){
-      console.log(x[i].product.title);
-    }
-  }
-}
-moreThanOneImage(productsItems);
-
-//Q4
-var productsItems = products.items;
-function allCanon(x) {
-  for (var i = 0; i < x.length; i++){
-    if (x[i].product.brand.indexOf("Canon") >= 0){
-      console.log(x[i].product.title);
-    }
-  }
-}
-allCanon(productsItems);
-
-//Q5
-var productsItems = products.items;
-function printItems(x) {
-  for (var i = 0; i < x.length; i++){
-    if (x[i].product.author.name.indexOf("eBay") >= 0 && x[i].product.brand.indexOf("Canon") >= 0){
-      console.log(x[i]);
-    }
-  }
-}
-printItems(productsItems);
-
-
-//Q6
-var productsItems = products.items;
-function printProductsWithBrandPriceImage(x) {
-  for (var i = 0; i < x.length; i++){
-      console.log(x[i].product.brand);
-      console.log(x[i].product.inventories[0].price);
-      console.log(x[i].product.images);
-    }
-}
-printProductsWithBrandPriceImage(productsItems);
-
-*/
-//continuation of excercise
-//Q1
 function getItems(func) {
   return func.items;
 }
@@ -123,13 +37,17 @@ function getAvailableProducts(instock){
   return availableItems;
 }
 
+
 //Q5
 
+//pt.1
 var items = getItems(products);
 var sonyItems = getItemsByBrand("Sony");
 //console.log(sonyItems);
 
-function getAvailableBrandProducts(brand,instock){
+//pt.2
+/*
+function getAvailableBrandProducts(brand, instock){
   var availableBrandProducts = []
   for (var i = 0; i < items.length; i++){
     if ((items[i].product.brand.toLowerCase() == brand.toLowerCase()) && (items[i].product.inventories[0].availability.toLowerCase() == instock.toLowerCase())){
@@ -139,9 +57,30 @@ function getAvailableBrandProducts(brand,instock){
   return availableBrandProducts;
 }
 
-var sonyavail = getAvailableBrandProducts("sony","instock");
-console.log(sonyavail);
+var sonyInstock = getAvailableBrandProducts("sony","instock");
+console.log(sonyInstock);
+*/
 
+//pt.3
+/*
+var availableItemsByAdorama = getItemsByAuthor("adorama camera")
+console.log(availableItemsByAdorama);
+*/
+
+//pt.4
+
+function getBrandAndAuthor(brand, author) {
+  var brandAndAuthorItems = []
+  for (var i = 0; i < items.length; i++){
+    if (items[i].product.brand.toLowerCase() == brand.toLowerCase() && items[i].product.author["name"].indexOf(author) >= 0){
+      brandAndAuthorItems.push(items[i]);
+    }
+  }
+  return brandAndAuthorItems;
+}
+
+var authorInstock = getBrandAndAuthor("nikon", "eBay");
+console.log(authorInstock);
 
 
 
