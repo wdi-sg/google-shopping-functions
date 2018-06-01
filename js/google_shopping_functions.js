00dfc4f6a840a695ba140f7b496fcfd74e1a2b81
jellythewobbly <jellythewobbly@hotmail.com>
2018-06-02 01:40:14 +0800

    Completed functions for JSON

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,17 +1,84 @@
+// Part 1
+// getItems(objectData)
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+var getItems = function() {
+	return products.items;
 }
 
-/*
- * Define and use your functions here
- */
+// To test if the function works
+// console.log( getItems() );
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+
+
+// Part 2
+// getItemsByBrand(items, brand)
+
+var getItemsByBrand = function(items, brand) {
+	var holder = [];
+	for ( var i = 0; i < items.length; i++ ) {
+		if ( items[i].product.brand.toLowerCase() == brand.toLowerCase() ) {
+			holder.push( items[i] );
+		}
+	} return holder;
+}
+
+// To test if the function works
+// console.log( getItemsByBrand(getItems(),'sony') );
+
+
+
+// Part 3
+// getItemsByAuthor(items, author)
+
+var getItemsByAuthor = function(items, author) {
+	var holder = [];
+	for ( var i = 0; i < items.length; i++ ) {
+		// Checking if object author name includes value passed as argument
+		if ( items[i].product.author.name.toLowerCase().includes( author.toLowerCase() ) == true ) {
+			holder.push ( items[i] );
+		}
+	} return holder;
+}
+
+// To test if the function works
+// console.log( getItemsByAuthor(getItems(),'ebay') );
+
+
+
+// Part 4
+// getAvailableProducts(items)
+
+var getAvailableProducts = function(items) {
+	var holder = [];
+	for ( var i = 0; i < items.length; i++ ) {
+		if ( items[i].product.inventories[0].availability.toLowerCase() == "instock" ) {
+			holder.push ( items[i] );
+		}
+	} return holder;
+}
+
+// To test if the function works
+// console.log(getAvailableProducts( getItems() ) );
+
+
+
+// Part 5
+// Use your functions
+
+// * All items made by Sony. (Answer = 5)
+console.log ( getItemsByBrand( getItems(),'sony' ));
+
+
+// * All items made by Sony that are available. (Answer = 4)
+console.log( getAvailableProducts( getItemsByBrand( getItems(),'sony' )));
+
+
+// * All available items by the author "Adorama Camera" (Answer = 0)
+console.log( getAvailableProducts(( getItemsByAuthor( getItems(),'adorama camera' ))));
+
+
+// * All items made by Nikon with the author eBay. (Answer = 2)
+console.log( getItemsByBrand(( getItemsByAuthor( getItems(), 'ebay' )) , 'Nikon' ));
+
+
 
