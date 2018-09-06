b64069e39148c42ea36ff228eb577cfbccca8783
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-06 17:05:41 +0800

    Complete 5. use your functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var items = getItems(products);
 var searchBrands = ['Sony', 'Canon', 'Nikon', 'Panasonic'];
 var searchAuthors = ['Target', 'CDW', 'eBay'];
+var searchString;
+var availableItems = getAvailableProducts(items);
 
 console.log('This is the test for getItems(objectData)');
 console.log(items);
@@ -20,3 +22,19 @@ for (i in searchAuthors)
 console.log('');
 console.log('This is the test for getAvailableProducts(items)');
 console.log(getAvailableProducts(items));
+
+console.log('');
+console.log('1. All items made by Sony:');
+console.log(getItemsByBrand(items,'Sony'));
+
+console.log('');
+console.log('2. All items made by Sony that are available:');
+console.log(getItemsByBrand(availableItems,'Sony'));
+
+console.log('');
+console.log('3. All available items by the author “Adorama Camera":');
+console.log(getItemsByAuthor(availableItems,'Adorama Camera'));
+
+console.log('');
+console.log('4. All items made by Nikon with the author eBay:');
+console.log(getItemsByAuthor(getItemsByBrand(items, 'Nikon'),'eBay'));
