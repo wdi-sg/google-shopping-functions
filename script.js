0dcdab6fd4b2ea9d66ca84a216ec0c4312256793
weiwenl <weiwenl.justin@gmail.com>
2018-09-06 17:12:07 +0800

    Remove accidental creation of a global var in func

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //1
 var getItems = function(productData){
-  return productsArray = productData.items; //RETURNS array of items
+  return productData.items; //RETURNS array of items
 }
 
-getItems(products); //INPUTS the 'thing' containing the items
+var productsArray  = getItems(products); //INPUTS the 'thing' containing the items
 console.log(productsArray);
