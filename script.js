ade254f82c49028f5ed59bac363ad6797e251d3c
weiwenl <weiwenl.justin@gmail.com>
2018-09-06 17:20:04 +0800

    Q2

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ var getItems = function(productData){
   return productData.items; //RETURNS array of items
 }
 
-var productsArray  = getItems(products); //INPUTS the 'thing' containing the items
+var productsArray = getItems(products); //INPUTS the 'thing' containing the items
 console.log(productsArray);
+
+//2
+var getItemsByBrand = function(productsArray, brand){
+  var brandArray = [];
+  for (var i = 0; i<productsArray.length; i++){
+    if(brand.toLowerCase() === productsArray[i].product.brand.toLowerCase()){
+      brandArray.push(productsArray[i]);
+    }
+  }
+  return brandArray;
+}
+
+var brandArray = getItemsByBrand(productsArray, "canon");
+console.log(brandArray);
