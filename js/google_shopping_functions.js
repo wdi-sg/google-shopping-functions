2848529db73f9e602a3ef3d4bcdbb7b4e1ccc3d4
mervyns <mervyns@gmail.com>
2018-09-06 16:40:19 +0800

    Finished up to Part 2 of Further Exercises

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,4 +1,3 @@
-
 /*
  * example function called getItemsCount
  * input: accepts full item data
@@ -11,7 +10,43 @@ function getItemsCount(itemData) {
 /*
  * Define and use your functions here
  */
+// Part 1 Create a function called getItems that simply returns the items array from the google product object.
+var getItems = function(objectData) {
+  return objectData.items
+}
 
+// Part 2 Create a function called getItemsByBrand that takes an item array returns a new array of all items of a specified brand.
+var getItemsByBrand = function (items, brand) {
+	var brandedItemsArray = [];
+ 	for (var i = 0; i < items.length ; i++) {
+		if (items[i].product.brand.toLowerCase() == brand.toLowerCase()) {
+			brandedItemsArray.push(items[i]);
+		};
+	};
+ 	return brandedItemsArray;
+};
+
+// Part 3 Create a function called getItemsByAuthor that takes an item array and returns a new array of all items by a specified author.
+var getItemsByAuthor = function(items, author) {
+	var authorArray = [];
+	for (i = 0; i < items.length; i++) {
+	// I used the method .includes as there were certain authors that were not an exact string to ebay.
+		if (items[i].product.author.name.toLowerCase().includes(author.toLowerCase())) {
+			authorArray.push(items[i]);
+		};
+	};
+	return authorArray;
+};
+
+// Part 4 Create function called getAvailableProducts that takes an item array and returns an array containing all of the available products 
+var getAvailableProducts = function(items) {
+	var availableProductsArray = [];
+	for (i=0; i<items.length; i++) {
+		if(items[i].product.inventories[0].availability == "inStock") {
+			availableProductsArray.push(items[i]);
+		};
+	};
+	return availableProductsArray;
+}
 // output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
-
+// console.log('Item Count: ' + getItemsCount(data));
---
+++ script.js
@@ -0,0 +1,51 @@
+// Part 1 Call
+console.log(getItems(products));
+// Part 2 Call
+console.log(getItemsByBrand(getItems(products), "Nikon"));
+// Part 3 Call
+console.log(getItemsByAuthor(getItems(products), "Target"));
+console.log(getItemsByAuthor(getItems(products), "CDW"));
+console.log(getItemsByAuthor(getItems(products), "Ebay"));
+// Part 4 Call
+console.log(getAvailableProducts(getItems(products)));
+// Part 5 Call
+var allItems = getItems(products);
+// Get all items made by Sony
+console.log(getItemsByBrand(allItems, "Sony"))
+// Get all items made by Sony and is available
+console.log(getAvailableProducts(getItemsByBrand(allItems,"Sony")));
+// Get all available items by the author "Adorama Camera"
+console.log(getAvailableProducts(getItemsByAuthor(allItems,"Adorama Camera")));
+// Get all items made by Nikon with the author eBay.
+console.log(getItemsByAuthor(getItemsByBrand(allItems, "Nikon"), "eBay"));
+// Further Exercises prompt the user what to search for
+var searchTerm = prompt("What do you want to search for? Brand, Author or Availability?")
+if (searchTerm.toLowerCase() == "brand") {
+	var searchBrand = prompt("What brand would you like to search for?")
+	var searchResultByBrand = getItemsByBrand(allItems, searchBrand.toLowerCase())
+// Ensures that the product is returned only if products were found. otherwise return message that no products were found.
+	if (searchResultByBrand > 0) {
+	console.log(searchResultByBrand);
+} else {
+	console.log("No results were found for your brand search.")
+}
+} else if (searchTerm.toLowerCase() == "author") {
+	var searchAuthor = prompt("Which author would you like to search for?")
+	var searchResultByAuthor = getItemsByAuthor(allItems, searchAuthor)
+	if (searchResultByAuthor > 0) {
+		console.log(searchResultByAuthor);
+	} else {
+		console.log("No results were found for that particular author")
+	}
+} else if (searchTerm.toLowerCase() == "availability") {
+	var searchResultsByAvailability = getAvailableProducts(allItems)
+	if (searchResultsByAvailability > 0) {
+		console.log(searchResultsByAvailability);
+	} else {
+		console.log("No items were available. All items are on backorder.")
+	}
+} else {
+	console.log("Please key in a valid value");
+};
+
+// Further 2 Exercises 
