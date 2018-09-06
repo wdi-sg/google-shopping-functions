b57b3a4615b1b9e8c31b435c4e7c0966864d91c4
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-06 17:45:35 +0800

    Complete user prompts for further

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var items = getItems(products);
 var searchBrands = ['Sony', 'Canon', 'Nikon', 'Panasonic'];
 var searchAuthors = ['Target', 'CDW', 'eBay'];
-var searchString;
+var searchCat;
+var searchString, userPrompt, userPref;
 var availableItems = getAvailableProducts(items);
 
 console.log('This is the test for getItems(objectData)');
@@ -29,7 +30,8 @@ console.log(getItemsByBrand(items,'Sony'));
 
 console.log('');
 console.log('2. All items made by Sony that are available:');
-console.log(getItemsByBrand(availableItems,'Sony'));
+console.log(getItemsByBrand(getAvailableProducts(getItems(products
+)),'Sony'));
 
 console.log('');
 console.log('3. All available items by the author “Adorama Camera":');
@@ -38,3 +40,19 @@ console.log(getItemsByAuthor(availableItems,'Adorama Camera'));
 console.log('');
 console.log('4. All items made by Nikon with the author eBay:');
 console.log(getItemsByAuthor(getItemsByBrand(items, 'Nikon'),'eBay'));
+
+//further 1
+console.log('');
+console.log('User Search Results:');
+userPrompt = prompt('Please enter a search category:', 'Availability, Author, Brand').toLowerCase();
+searchCat = ['availability', 'author', 'brand'];
+if (userPrompt === searchCat[0])
+  console.log(getAvailableProducts(items));
+else if (userPrompt === searchCat[1]) {
+    userPref = prompt("Enter the author's name:");
+    console.log(getItemsByAuthor(items, userPref));
+} else if (userPrompt === searchCat[2]) {
+    userPref = prompt("Enter the brand's name:");
+    console.log(getItemsByBrand(items, userPref));
+} else
+  console.log('Invalid input.')
