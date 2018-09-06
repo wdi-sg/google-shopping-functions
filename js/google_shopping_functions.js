16e7c840a101986f0d438cada43a999beb0381a9
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-06 20:32:54 +0800

    google-shopping-function assignment part 1-5 + further

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,17 +1,105 @@
-
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+//Part 1: Create a function called getItems that simply returns the items array from the google
+var getItems = function(productsObject){
+    for (var i = 0; i < productsObject.items.length; i++){
+      var short_item = productsObject.items;
+      //console.log(short_item[i]);
+    }
+    return short_item;
 }
 
-/*
- * Define and use your functions here
- */
+//Part 2: Create a function called getItemsByBrand that takes an item array returns a new array of all items of a specified brand.
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+var getItemsByBrand = function(short_item, brand){
+  var short_item_brand = [];
+  for (var i = 0; i < short_item.length; i++){
+      if (short_item[i].product.brand === brand){
+      short_item_brand.push(short_item[i]);
+      //console.log(short_item_brand);
+    }
+  }
+  return short_item_brand;
+}
+
+//Part 3: Create a function called getItemsByAuthor that takes an item array and returns a new array of all items by a specified author.
+var getItemsByAuthor = function(short_item, author){
+  var short_item_author = [];
+  for (var i = 0; i < short_item.length; i++){
+      if (short_item[i].product.author.name.includes(author)){
+      short_item_author.push(short_item[i]);
+      //console.log(short_item_author);
+    }
+  }
+  return short_item_author;
+}
+
+//Part 4: Create function called getAvailableProducts that takes an item array and
+//returns an array containing all of the available products (an available product is one with at least one availability of "inStock"
+//in the inventories array
+var getAvailableProducts = function(short_item){
+  var short_item_avail = [];
+  for (var i = 0; i <short_item.length; i++){
+    for (var j = 0; j< short_item[i].product.inventories.length; j++){
+      if (short_item[i].product.inventories[j].availability === "inStock"){
+        short_item_avail.push(short_item[i]);
+        //console.log(short_item_avail);
+      }
+    }
+  }
+  return short_item_avail;
+}
+
+//Further:
+var getCountry = function(short_item){
+  var short_item_country = [];
+  var dup_short_item_country = [];
+  for (var i = 0; i < short_item.length; i++){
+      short_item_country.push(short_item[i].product.country);
+      for (var j = 0; j < short_item_country.length; j++){
+        if (short_item_country[j] === short_item_country[j + 1]){
+          dup_short_item_country = short_item_country[j];
+        }
+        //else if (short_item_country[j] != short_item_country[j + 1]){
+          //dup_short_item_country.push(short_item_country[j]);
+        //}
+        //unsure how to proceed with the non-duplicates.
+
+      }
+  }
+  console.log(dup_short_item_country)
+}
+
+
+
+var getPrice = function(short_item){
+  var total_price = 0;
+  for (var i = 0; i < short_item.length; i++){
+    for (var j=0; j< short_item[i].product.inventories.length; j++){
+      total_price = total_price + short_item[i].product.inventories[j].price;
+    }
+  }
+  console.log(total_price);
+}
+
+//additional search function: get link of same brand product
+var getLink = function(short_item){
+  var link = [];
+  for (var i =0; i< short_item.length; i++){
+    console.log(short_item[i].product.link);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ script.js
@@ -0,0 +1,66 @@
+
+//Part 1: Create a function called getItems that simply returns the items array from the google product object.
+var short_item = getItems(products);
+
+//Part 2: Create a function called getItemsByBrand that takes an item array returns a new array of all items of a specified brand.
+console.log("---------------------------------------------------------------------");
+//getItemsByBrand(short_item, "Nikon");
+
+//Part 3: Create a function called getItemsByAuthor that takes an item array and returns a new array of all items by a specified author.
+console.log("---------------------------------------------------------------------");
+//getItemsByAuthor(short_item, "CDW");
+
+
+//Part 4: Create function called getAvailableProducts that takes an item array and
+//returns an array containing all of the available products (an available product is one with at least one availability of "inStock"
+//in the inventories array
+console.log("---------------------------------------------------------------------");
+//getAvailableProducts(short_item);
+
+//Part 5: Using the functions
+console.log("Print all items made by Sony");
+//All items made by Sony.
+//console.log(getItemsByBrand(short_item, "Sony"));
+
+console.log("Print all items made by Sony and is available (in stock)");
+//All items made by Sony that are available.
+//console.log(getItemsByBrand(getAvailableProducts(short_item), "Sony"));
+console.log("Print all available items by author Adorama Camera");
+
+//All available items by the author "Adorama Camera" //Should be empty array cause no results.
+//console.log(getItemsByAuthor(getAvailableProducts(short_item), "Adorama Camera"));
+
+console.log("Print all items made by Nikon with author eBay");
+//All items made by Nikon with the author eBay.
+//console.log(getItemsByAuthor(getItemsByBrand(short_item, "Nikon"), "eBay"));
+
+//Further:
+//Ask the user which one they want to search: Number = number of products item, Country = country of each item, Price = total price of all inventory.
+/*
+var searchTerm = prompt("Please insert what you are searching for: (Number, Country, Price) ");
+var searchTerm_lowercase = searchTerm.toLowerCase();
+if (searchTerm_lowercase === "number"){
+  console.log(short_item.length);
+}
+else if (searchTerm_lowercase === "country"){
+  getCountry(short_item);
+}
+else if (searchTerm_lowercase === "price"){
+  getPrice(short_item);
+}
+else {
+  console.log("No results found!");
+}
+*/
+//Using additional function:
+getLink(getItemsByBrand(short_item, "Sony"));
+
+
+
+
+
+
+
+
+
+
