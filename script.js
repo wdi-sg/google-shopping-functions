05f2d9b61696464aeb2b4e8ef8e90e847a754c48
xnithunx <nithun.nanda@gmail.com>
2018-09-07 08:54:50 +0800

    partial complete

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-//part 1
-console.log(getItems(products));
+//Part 1
+ var itemArray = getItem(products);
+  console.log(itemArray);
+
+//part 2
+
+var brandArray = getItemsByBrand(itemArray, "Canon");
+console.log(brandArray);
+
+//part 3
+
+var authorArray = getItemsByAuthor(itemArray, "eBay");
+console.log(authorArray);
 
