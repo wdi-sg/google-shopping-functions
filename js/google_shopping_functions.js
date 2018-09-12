de31260b724e4b8d613a286f2e55305da7305fda
traviegotswag <travisng@hotmail.sg>
2018-09-12 12:55:12 +0800

    Assignment 4 - 6 Sep 2018

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,17 +1,56 @@
+// 1.Create a function called getItems that simply returns the items array from the google product object.
+//function is meant to be dynamic, can be called from various places
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+function getItems(obj) {
+  var newArray = [];
+  for (i = 0; i < obj.items.length; i++) {
+    newArray.push(obj.items[i]);
+  };
+  return newArray;
 }
 
-/*
- * Define and use your functions here
- */
+// getItems(products);
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+getItemsByBrand(getItems(products), 'Sony')
 
+// 2. Create a function called getItemsByBrand that takes an item array and returns a new array of all items of a specified brand.
+
+function getItemsByBrand(items, specifiedBrand) {
+  var items = items
+  var result = []
+  for (i = 0; i < items.length; i++) {
+    if (items[i].product.brand === specifiedBrand) {
+      result.push(items[i]);
+    }
+  }
+  return result;
+}
+
+// getItemsByBrand(specifiedBrand);
+
+// 3. Create a function called getItemsByAuthor that takes an item array and returns a new array of all items by a specified author.
+
+function getItemsByAuthor(specifiedAuthor) {
+  var items = products.items
+  var result = [];
+  for (i = 0; i < items.length; i++) {
+    if (products.items[i].product.author.name === specifiedAuthor) {
+      result.push(products.items[i]);
+    }
+  }
+  return result;
+}
+// getItemsByBrand(specifiedAuthor);
+
+// 4. Create function called getAvailableProducts that takes an item array and returns an array containing all of the available products (an available product is one with at least one availability of "inStock" in the inventories array)
+function getAvailableProducts(availability) {
+  var items = products.items
+  var result = []
+  for (i = 0; i < items.length; i++) {
+    if (items[i].product.inventories[0].availability === "inStock")
+      result.push(items[i].product);
+  }
+  return result;
+}
+
+// getItemsByBrand(availability);
---
+++ script.js
@@ -0,0 +1,13 @@
+// // 5a. All items made by Sony.
+
+
+// 5b. All items made by Sony that are available.
+
+
+// 5c. All available items by the author "Adorama Camera"
+
+
+// 5d. All items made by Nikon with the author eBay.
+
+
+// 5e. remember that you must create a script tag for each file you create, and that they must be in the correct order for your code to run.
