f5b554ca4eeb0389a753086190b0b6580b3c4abc
traviegotswag <travisng@hotmail.sg>
2018-09-13 09:14:01 +0800

    Updated assignment

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,56 +1,58 @@
 // 1.Create a function called getItems that simply returns the items array from the google product object.
 //function is meant to be dynamic, can be called from various places
 
-function getItems(obj) {
-  var newArray = [];
-  for (i = 0; i < obj.items.length; i++) {
-    newArray.push(obj.items[i]);
-  };
-  return newArray;
+var getItems = function(obj) {
+  return products.items
 }
 
-// getItems(products);
+// console.log(getItems(products))
 
-getItemsByBrand(getItems(products), 'Sony')
+var itemsList = getItems(products);
 
-// 2. Create a function called getItemsByBrand that takes an item array and returns a new array of all items of a specified brand.
+// // 2. Create a function called getItemsByBrand that takes an item array and returns a new array of all items of a specified brand.
 
-function getItemsByBrand(items, specifiedBrand) {
-  var items = items
-  var result = []
-  for (i = 0; i < items.length; i++) {
-    if (items[i].product.brand === specifiedBrand) {
-      result.push(items[i]);
+var getItemsByBrand = function(itemsInput, specifiedBrand) {
+  var brandArray = [];
+  for (var i = 0; i < itemsInput.length; i++) {
+    if (itemsInput[i].product.brand === specifiedBrand) {
+      brandArray.push(itemsInput[i]);
     }
   }
-  return result;
+  return brandArray
 }
 
-// getItemsByBrand(specifiedBrand);
+// //Test this function by searching for Sony, Canon, Nikon and Panasonic.
+
+console.log(getItemsByBrand(itemsInput,"Sony"))
+console.log(getItemsByBrand(itemsInput,"Canon"))
+console.log(getItemsByBrand(itemsInput,"Nikon"))
+console.log(getItemsByBrand(itemsInput,"Panasonic"))
 
 // 3. Create a function called getItemsByAuthor that takes an item array and returns a new array of all items by a specified author.
 
-function getItemsByAuthor(specifiedAuthor) {
-  var items = products.items
-  var result = [];
-  for (i = 0; i < items.length; i++) {
-    if (products.items[i].product.author.name === specifiedAuthor) {
-      result.push(products.items[i]);
+var getItemsByAuthor = function (itemsInput, specifiedAuthor) {
+    var authorArray = [];
+    for (var i = 0; i < itemsInput.length; i++) {
+      if (itemsInput[i].product.author.name === specifiedAuthor) {
+      authorArray.push(itemsInput[i]);
     }
   }
-  return result;
+  return authorArray
 }
-// getItemsByBrand(specifiedAuthor);
 
-// 4. Create function called getAvailableProducts that takes an item array and returns an array containing all of the available products (an available product is one with at least one availability of "inStock" in the inventories array)
-function getAvailableProducts(availability) {
-  var items = products.items
-  var result = []
-  for (i = 0; i < items.length; i++) {
-    if (items[i].product.inventories[0].availability === "inStock")
-      result.push(items[i].product);
+// // Test this function by searching for Target, CDW, eBay
+// console.log(getItemsByAuthor(getItems(products),"Target"))
+// console.log(getItemsByAuthor(getItems(products),"CDW"))
+// console.log(getItemsByAuthor(getItems(products),"eBay"))
+
+// // 4. Create function called getAvailableProducts that takes an item array and returns an array containing all of the available products (an available product is one with at least one availability of "inStock" in the inventories array)
+
+var getAvailableProducts = function (itemsInput) {
+  var availableProductsArray = [];
+  for (var i = 0; i < itemsInput.length; i++) {
+    if (itemsInput[i].product.inventories[0].availability === "inStock") {
+      availableProductsArray.push(itemsInput[i].product);
+    }
   }
-  return result;
+  return availableProductsArray
 }
-
-// getItemsByBrand(availability);
--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // // 5a. All items made by Sony.
-
+console.log(getItemsByBrand(getItems,'Sony'))
 
 // 5b. All items made by Sony that are available.
 
+var sonyItems = getItemsByBrand(getItems, 'Sony');
+console.log(getAvailableProducts(sonyItems))
 
 // 5c. All available items by the author "Adorama Camera"
 
+var AdoramaCameraItems = getItemsByAuthor(getItems,"Adorama Camera");
+console.log(getAvailableProducts(AdoramaCameraItems))
 
 // 5d. All items made by Nikon with the author eBay.
 
+var nikonItems = getItemsByBrand(getItems, 'Nikon');
+console.log(getItemsByAuthor(nikonItems,"Adorama Camera")
 
 // 5e. remember that you must create a script tag for each file you create, and that they must be in the correct order for your code to run.
