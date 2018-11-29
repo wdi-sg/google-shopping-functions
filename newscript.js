9c08674433d77b29e4d1be47351d1ec76b2c0c69
sean chan <sean@seans-MacBook-Air.local>
2018-11-29 23:29:06 +0800

    Further part 1-3

--- newscript.js
+++ newscript.js
@@ -216,102 +216,183 @@
 
 // * remember that you must create a script tag for each file you create, and that they must be in the correct order for your code to run.
 
-var getItems = function(){
-  return products.items;
-}
+// var getItems = function(){
+//   return products.items;
+// }
 
-var items = products.items;
-// Code below search and print all items made by Sony.
+// var items = products.items;
+// // Code below search and print all items made by Sony.
 
-var getItemsByBrand = function(items, brand){
-  var itemBrand = brand;
-  var itemArray = items;
-  var sonyArray = new Array();
-  var canonArray = new Array();
-  var lytroArray = new Array();
-  var nikonArray = new Array();
-  var samsungArray = new Array();
-  var fujiArray = new Array();
-  var olympusArray = new Array();
-  var pentraxArray = new Array();
+// var getItemsByBrand = function(items, brand){
+//   var itemBrand = brand;
+//   var itemArray = items;
+//   var sonyArray = new Array();
+//   var canonArray = new Array();
+//   var lytroArray = new Array();
+//   var nikonArray = new Array();
+//   var samsungArray = new Array();
+//   var fujiArray = new Array();
+//   var olympusArray = new Array();
+//   var pentraxArray = new Array();
 
-  for(var i = 0; i < itemArray.length; i++){
-  var numItems = itemArray[i];
-  var productBrand = numItems.product.brand
-    if(itemBrand.toLowerCase() === "sony" && productBrand.toLowerCase() === "sony") {
-      sonyArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "canon" && productBrand.toLowerCase() === "canon") {
-      canonArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "lytro" && productBrand.toLowerCase() === "lytro") {
-      lytroArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "nikon" && productBrand.toLowerCase() === "nikon") {
-      nikonArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "samsung" && productBrand.toLowerCase() === "samsung") {
-      samsungArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "fuji" && productBrand.toLowerCase() === "fuji") {
-      fujiArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "olympus" && productBrand.toLowerCase() === "olympus") {
-      olympusArray.push(numItems.product);
-    } else if(itemBrand.toLowerCase() === "pentrax" && productBrand.toLowerCase() === "pentrax") {
-      pentraxArray.push(numItems.product);
-    }
-  }
+//   for(var i = 0; i < itemArray.length; i++){
+//   var numItems = itemArray[i];
+//   var productBrand = numItems.product.brand
+//     if(itemBrand.toLowerCase() === "sony" && productBrand.toLowerCase() === "sony") {
+//       sonyArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "canon" && productBrand.toLowerCase() === "canon") {
+//       canonArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "lytro" && productBrand.toLowerCase() === "lytro") {
+//       lytroArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "nikon" && productBrand.toLowerCase() === "nikon") {
+//       nikonArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "samsung" && productBrand.toLowerCase() === "samsung") {
+//       samsungArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "fuji" && productBrand.toLowerCase() === "fuji") {
+//       fujiArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "olympus" && productBrand.toLowerCase() === "olympus") {
+//       olympusArray.push(numItems.product);
+//     } else if(itemBrand.toLowerCase() === "pentrax" && productBrand.toLowerCase() === "pentrax") {
+//       pentraxArray.push(numItems.product);
+//     }
+//   }
 
-    if(itemBrand.toLowerCase() === "sony"){
-      return sonyArray;
-    } else if(itemBrand.toLowerCase() === "canon"){
-      return canonArray;
-    } else if(itemBrand.toLowerCase() === "lytro"){
-      return lytroArray;
-    } else if(itemBrand.toLowerCase() === "nikon"){
-      return nikonArray;
-    } else if(itemBrand.toLowerCase() === "samsung"){
-      return samsungArray;
-    } else if(itemBrand.toLowerCase() === "fuji"){
-      return fujiArray;
-    } else if(itemBrand.toLowerCase() === "olympus"){
-      return olympusArray;
-    } else if(itemBrand.toLowerCase() === "pentrax"){
-      return pentraxArray;
-    }
-  else{
-    console.log("No such brand");
-  }
-}
+//     if(itemBrand.toLowerCase() === "sony"){
+//       return sonyArray;
+//     } else if(itemBrand.toLowerCase() === "canon"){
+//       return canonArray;
+//     } else if(itemBrand.toLowerCase() === "lytro"){
+//       return lytroArray;
+//     } else if(itemBrand.toLowerCase() === "nikon"){
+//       return nikonArray;
+//     } else if(itemBrand.toLowerCase() === "samsung"){
+//       return samsungArray;
+//     } else if(itemBrand.toLowerCase() === "fuji"){
+//       return fujiArray;
+//     } else if(itemBrand.toLowerCase() === "olympus"){
+//       return olympusArray;
+//     } else if(itemBrand.toLowerCase() === "pentrax"){
+//       return pentraxArray;
+//     }
+//   else{
+//     console.log("No such brand");
+//   }
+// }
 
-var getItemsByAuthor = function(items, author) {
-    var newItems = [];
-    var itemArray = items;
-    var authorName = author.toLowerCase();
+// var getItemsByAuthor = function(items, author) {
+//     var newItems = [];
+//     var itemArray = items;
+//     var authorName = author.toLowerCase();
 
-  for(var i = 0; i < itemArray.length; i++){
-    var numItems = products.items[i];
-    var productAuthor = numItems.product.author.name;
-    var lowerAuthor = productAuthor.toLowerCase();
-      if (lowerAuthor.includes(author) === true) {
-           newItems.push(products.items[i]);
-        }
-  }
-    return newItems;
-}
+//   for(var i = 0; i < itemArray.length; i++){
+//     var numItems = products.items[i];
+//     var productAuthor = numItems.product.author.name;
+//     var lowerAuthor = productAuthor.toLowerCase();
+//       if (lowerAuthor.includes(author) === true) {
+//            newItems.push(products.items[i]);
+//         }
+//   }
+//     return newItems;
+// }
 
-// Code below search and print all items made by Sony that are available. getItemsByAvailablity(getItems(), "instock");
+// // Code below search and print all items made by Sony that are available. getItemsByAvailablity(getItems(), "instock");
 
-var getItemsByAvailablity = function(available) {
-    var newItems = [];
+// var getItemsByAvailablity = function(available) {
+//     var newItems = [];
 
-  for(var i = 0; i < products.items.length; i++){
-    var numItems = products.items[i];
-    var productAvailablity = numItems.product.inventories[0].availability;
-      if (productAvailablity.toLowerCase() === "instock") {
-           newItems.push(products.items[i]);
-        }
-  }
-    return newItems;
-}
+//   for(var i = 0; i < products.items.length; i++){
+//     var numItems = products.items[i];
+//     var productAvailablity = numItems.product.inventories[0].availability;
+//       if (productAvailablity.toLowerCase() === "instock") {
+//            newItems.push(products.items[i]);
+//         }
+//   }
+//     return newItems;
+// }
 
 // console.log(getItemsByAuthor(getItemsByAvailablity(items), "adorama camera"));
 // console.log(getItemsByBrand(getItemsByAuthor(items, "ebay"), "nikon"));
 
 // availAuthor(getItems(), "adorama camera");
 // getItemsByAvailablity(getItems(products), "instock");
+
+
+// ## Further Part 1 - 3
+// Prompt the user for the search term they are looking for.
+// If no results were found, let the user know.
+// Prompt the user if they want to do or see the following things, then do that thing based on what the user entered:
+
+// 1) The number of product items
+
+// 2) The country of each item
+
+// 3) Total price of all inventory
+
+// 4) Search for one of the things above
+
+var userInput = prompt("Please input the number you like to search for \n 1)The number of product items \n 2)The country of each item \n 3)Total price of all inventory \n 4)Search randomly for one of the things above");
+var userInputSearch = parseInt(userInput);
+
+function getSum(total, num) {
+    return total + num;
+}
+
+if(userInputSearch === 1){
+    var countItems = 0;
+
+      for(var i = 0; i < products.items.length; i++){
+        var numItems = products.items[i]; // Number of array in items
+        if(numItems == numItems){
+          countItems += 1;
+        }
+      }
+    console.log("The total number of product items are: " +countItems);
+} else if(userInputSearch === 2){
+      for(var i = 0; i < products.items.length; i++){
+        var numItems = products.items[i];
+          console.log("The name of each product: " + numItems.product.title + " "+ "----->" + " " + "The country of Origin: " + numItems.product.country);
+      }
+} else if(userInputSearch === 3){
+    var totalPrice = [];
+      for(var i = 0; i < products.items.length; i++){
+        var numItems = products.items[i];
+        var priceItems = numItems.product.inventories[0].price;
+        totalPrice.push(priceItems);
+      }
+      console.log("The total price of all inventory in USD: " + "$" + parseFloat(Math.round(totalPrice.reduce(getSum) * 100) / 100).toFixed(2));
+} else if(userInputSearch === 4){
+   var rand = Math.floor(Math.random() * 4);
+    if(rand === 1){
+          var countItems = 0;
+
+      for(var i = 0; i < products.items.length; i++){
+        var numItems = products.items[i]; // Number of array in items
+        if(numItems == numItems){
+          countItems += 1;
+        }
+      }
+    console.log("The total number of product items are: " +countItems);
+  } else if(rand === 2){
+      for(var i = 0; i < products.items.length; i++){
+        var numItems = products.items[i];
+          console.log("The name of each product: " + numItems.product.title + " "+ "----->" + " " + "The country of Origin: " + numItems.product.country);
+      }
+} else if(rand === 3){
+    var totalPrice = [];
+      for(var i = 0; i < products.items.length; i++){
+        var numItems = products.items[i];
+        var priceItems = numItems.product.inventories[0].price;
+        totalPrice.push(priceItems);
+      }
+      console.log("The total price of all inventory in USD: " + "$" + parseFloat(Math.round(totalPrice.reduce(getSum) * 100) / 100).toFixed(2));
+} else {
+  console.log("Refresh Browser");
+}
+}
+
+else{
+  console.log("GO away");
+}
+
+
+
