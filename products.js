1e7a0656decb0e0d8efdef16c2e5723dfc645e26
hongchiong <hongchiong@gmail.com>
2018-11-29 17:54:02 +0800

    prompt user inputs and search results by name brand country price

--- products.js
+++ products.js
@@ -1137,39 +1137,3 @@ var products = {
   }
  ]
 }
-
-// var count = 0;
-// for (var i in products.items) {
-//     if (products.items[i].kind == "shopping#product") {
-//         count++;
-//     }
-// }
-// console.log(count);
-
-// for (var i in products.items) {
-//     if (products.items[i].product.inventories[0].availability == "backorder") {
-//         console.log(products.items[i].product.title);
-//     }
-// }
-
-// for (var i in products.items) {
-//     if (products.items[i].product.images.length > 1) {
-//         console.log(products.items[i].product.title);
-//     }
-// }
-
-// for (var i in products.items) {
-//     if (products.items[i].product.brand == "Canon") {
-//         console.log(products.items[i].product.title);
-//     }
-// }
-
-// for (var i in products.items) {
-//     if ((products.items[i].product.brand == "Canon") && (products.items[i].product.author.name.includes("eBay"))) {
-//         console.log(products.items[i].product.title);
-//     }
-// }
-
-// for (var i in products.items) {
-//     console.log(products.items[i].product.brand + products.items[i].product.inventories[0].price + products.items[i].product.images[0].link);
-// }
--- script.js
+++ script.js
@@ -79,3 +79,62 @@ for (var i in nikonItems) {
     }
 }
 console.log(nikonEbayItems);
+
+
+
+
+var whatToDo = prompt("What do you want to do? Input the corresponding numbers. Add as many filters as you need, seperating the numbers with a space. (Eg. 1 2 3)\n1. Search by item name.\n2. Filter by Brand.\n3.Filter by Country.\n4. Filter by Price ")
+var result = [];
+if (whatToDo.includes("1")) {
+    var itemName = prompt("What is the item name?");
+    for (var i in products.items) {
+        if ((products.items[i].product.title.toLowerCase()).includes(itemName.toLowerCase())) {
+            result.push(products.items[i].product.title);
+        }
+    }
+}
+
+if (whatToDo.includes("2")) {
+    var brandFilter = prompt("What brand are you looking for? (Please only enter 1 brand)");
+    for (var i in products.items) {
+        if ((products.items[i].product.brand.toLowerCase()) == brandFilter.toLowerCase()) {
+            result.push(products.items[i].product.title);
+        }
+    }
+}
+
+if (whatToDo.includes("3")) {
+    var countryFilter = prompt("What country's products are you looking for?");
+    for (var i in products.items) {
+        if ((products.items[i].product.country.toLowerCase()) == countryFilter.toLowerCase()) {
+            result.push(products.items[i].product.title);
+        }
+    }
+}
+
+if (whatToDo.includes("4")) {
+    var minPrice = prompt("What is the minimum price?");
+    var maxPrice = prompt("What is the maximum price?");
+    if (products.items[i].product.inventories[0].price >= minPrice && products.items[i].product.inventories[0].price <= maxPrice) {
+        result.push(products.items[i].product.title);
+    }
+}
+
+
+var filterResults = function(result, numFilters) {
+    var filteredResults = [];
+    var counts = {}
+    for (var i = 0; i < result.length; i++) {
+        var filterItem = result[i];
+        counts[filterItem] = counts[filterItem] ? counts[filterItem] + 1 : 1;
+    }
+    for (var key in counts) {
+        if (counts[key] == numFilters) {
+            filteredResults.push("\n" + key + "\n");
+        }
+    }
+    return filteredResults;
+}
+
+alert(filterResults(result, (whatToDo.split(" ").map(Number)).length));
+
