0831aced47fcd5894601cea699507602ecfbe666
LYDIACYT <lydiacyt@hotmail.com>
2018-11-29 18:35:18 +0800

    completed deliverables

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -15,3 +15,56 @@ function getItemsCount(itemData) {
 // output item count using the getItemsCount function
 console.log('Item Count: ' + getItemsCount(data));
 
+//1
+
+function getItems(itemData) {
+  return products.items;
+}
+
+//2
+//items = products.item
+
+function getItemsByBrand(items,brand) {
+  var itemArray = [];
+
+  for (var i=0 ;i < products.items.length;i++){
+    if (products.items[i].products.brand == brand){
+      itemArray.push(products.items[i].product.title);
+    }
+
+
+  }
+
+  return itemArray;
+}
+
+//3
+
+function getItemsByAuthor(items,author) {
+  var authorArray = [];
+
+    for (var i=0 ;i < products.items.length;i++){
+    if (products.items[i].products.author[0] == author){
+      itemArray.push(products.items[i].product.title);
+      return authorArray;
+    }
+  }
+}
+
+//4
+
+function getAvailablePoints(items){
+
+  var availabilityArray = [];
+
+  for (var i=0 ;i < products.items.length;i++){
+    if (products.items[i].products.inventories[0].availability == "inStock"){
+      availabilityArray.push(products.items[i].product.title);
+      return availabilityArray;
+    }
+}
+
+var items = getItems(products);
+var cannonItems = getItemsByBrand(items,"Cannon");
+console.log(cannonItems);
+
