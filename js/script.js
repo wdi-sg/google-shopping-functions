cfb65092d37c301c6242c9c4a7fdc44aca14d994
Alan <alan88w@yahoo.com>
2018-11-29 23:21:21 +0800

    added alert when no result returns

--- js/script.js
+++ js/script.js
@@ -19,17 +19,17 @@ var nikonItems = getItemsByBrand(items, 'Nikon');
 var nikonOnEbay = getItemsByAuthor(nikonItems, "eBay");
 
 function getSearchTerm() {
-    var radioButton = document.getElementsByName("searchTerm");
-    var selectedValue;
+  var radioButton = document.getElementsByName("searchTerm");
+  var selectedValue;
 
-    if (radioButton[0].checked === true) {
-      selectedValue = radioButton[0].value;
-    } else {
+  if (radioButton[0].checked === true) {
+    selectedValue = radioButton[0].value;
+  } else {
 
-      selectedValue = radioButton[1].value;
-    }
+    selectedValue = radioButton[1].value;
+  }
 
-    return selectedValue;
+  return selectedValue;
 }
 
 function displayResult(resultList) {
@@ -37,32 +37,40 @@ function displayResult(resultList) {
   var outputDisplay = document.getElementById("search-result");
 
   // outputDisplay.insertAdjacentHTML('beforeend',  '<table style="width:100%"></table>');
-  for (i=0; i < resultList.length; i++) {
-    displayItem = '<p>'+(i+1)+". "+ resultList[i].product.title+' - $'+ resultList[i].product.inventories[0].price+'</p>'
+  for (i = 0; i < resultList.length; i++) {
+    displayItem = '<p>' + (i + 1) + ". " + resultList[i].product.title + ' - $' + resultList[i].product.inventories[0].price + '</p>'
     outputDisplay.insertAdjacentHTML('beforeend', displayItem);
   }
 
-//   outputDisplay.insertAdjacentHTML('beforeend',  '<tr><td>Jill</td><td>Smith</td><td>50</td></tr>)');
-//   outputDisplay.insertAdjacentHTML('beforeend',  '</table>');
+  //   outputDisplay.insertAdjacentHTML('beforeend',  '<tr><td>Jill</td><td>Smith</td><td>50</td></tr>)');
+  //   outputDisplay.insertAdjacentHTML('beforeend',  '</table>');
 }
 
 function searchInfo() {
-    var schTerm = getSearchTerm();
-    var schData = document.getElementById("searchData").value;
-    var searchResult=[];
+  var schTerm = getSearchTerm();
+  var schData = document.getElementById("searchData").value;
+  var searchResult = [];
+  var outputDisplay = document.getElementById("search-result");
 
-    if (schData.length <= 0) {
-      alert("Must enter a value in search string")
+  outputDisplay.innerHTML = "";
+
+  if (schData.length <= 0) {
+    alert("Must enter a value in search string")
+  } else {
+    if (schTerm === "byBrand") {
+      searchResult = getItemsByBrand(items, schData);
     } else {
-      if (schTerm === "byBrand") {
-          searchResult = getItemsByBrand(items, schData);
-      } else {
-          searchResult = getItemsByAuthor(items, schData);
+      searchResult = getItemsByAuthor(items, schData);
 
-      }
     }
+  }
 
+  if (searchResult.length < 1) {
+    alert("No result found")
+  } else {
     displayResult(searchResult);
+  }
+
 }
 
 var searchBtn = document.getElementById("searchBtn");
