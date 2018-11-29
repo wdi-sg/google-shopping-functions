68ebf4d490a9e35aa417839c0174600b98c70043
rchuaml <ronchuaml@gmail.com>
2018-11-29 23:46:05 +0800

    part 1-5 done

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -4,14 +4,59 @@
  * input: accepts full item data
  * output: returns the length of the items array
  */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+ /* part 1 */
+  function getItemsCount(itemData) {
+    return itemData.items.length;
+  }
+
+  function getItems(objectData){
+    return objectData.items;
+  }
+
+  function getItemsByBrand(items , brand){
+
+    var searchResults = [];
+
+    for(var i=0;i<items.length;i++){
+
+      if(items[i].product.brand===brand){
+
+        searchResults.push(       items[i]      );
+      }
+    }
+    return searchResults;
+  }
+
+  function getItemsByAuthor(items, author){
+
+    var searchResults2 = [];
+
+for(var i = 0; i<items.length; i++){
+        if(items[i].product.author.name===author){
+
+        searchResults2.push(items[i]);
+      }
+  }
+  return searchResults2;
 }
+
+function getAvailableProducts(items){
+var searchResults3 = [];
+
+for(var i = 0; i<items.length; i++){
+        if(items[i].product.inventories[0].availability==="inStock"){
+
+        searchResults3.push(items[i]);
+      }
+  }
+  return searchResults3;
+}
+
 
 /*
  * Define and use your functions here
  */
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+//  output item count using the getItemsCount function
+// console.log('Item Count: ' + getItemsCount(products));
 
