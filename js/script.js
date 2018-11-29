b815fe0e112ced8e41dfcb9d32fed86c02068160
Alan <alan88w@yahoo.com>
2018-11-29 23:21:50 +0800

    comment out the assignment

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ var items = getItems(products)
 
 // All items made by Sony.
 
-var sonyItems = getItemsByBrand(items, 'Sony');
+// var sonyItems = getItemsByBrand(items, 'Sony');
 // console.log(sonyItems);
 
 // All items made by Sony that are available.
 
-var availableSonyProducts = getAvailableProducts(sonyItems);
+// var availableSonyProducts = getAvailableProducts(sonyItems);
 
 // All available items by the author "Adorama Camera"
 
-var adoramaProducts = getItemsByAuthor(items, "Adorama Camera")
+// var adoramaProducts = getItemsByAuthor(items, "Adorama Camera")
 
 // All items made by Nikon with the author eBay.
 
-var nikonItems = getItemsByBrand(items, 'Nikon');
-var nikonOnEbay = getItemsByAuthor(nikonItems, "eBay");
+// var nikonItems = getItemsByBrand(items, 'Nikon');
+// var nikonOnEbay = getItemsByAuthor(nikonItems, "eBay");
 
 function getSearchTerm() {
   var radioButton = document.getElementsByName("searchTerm");
