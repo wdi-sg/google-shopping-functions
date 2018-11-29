ce3193fa1fdef996324b00781b9cc3ddb85e60c4
Ishak <ishak@Ishaks-MacBook-Pro.local>
2018-11-30 01:31:28 +0800

    Completed part 1-4, first part of part 5.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//Part 1
+console.log(getItems(products));
+
+//Part 2
+//Display all items by Canon
+console.log(getItemsByBrand(getItems(products), "Canon"));
+
+//Part 3
+//Display by Author CDW
+console.log(getItemsByAuthor(getItems(products), "CDW"));
+
+//Part 4
+//Display all items with availability inStock
+console.log(getAvailableProducts(getItems(products), "inStock"));
+
+//Part5
+
+//Display all items by Sony
+console.log(getItemsByBrand(getItems(products), "Sony"));
+
+
