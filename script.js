99eb107309d156c843ce4389de36c4b83fd5cce8
jonathanchow <jonathanchowjh@gmail.com>
2018-11-30 08:53:56 +0800

    submit2

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var items = products.items;
 
 //search by ... return array
 //###############################################################
+//get items by brand function
+//make array
+//iterate over database to check for equality
+//push into array
+//return array
 var getItemsByBrand = function(items,brandName) {
   var brandA = [];
   for(var i = 0; i < items.length; i++) {
@@ -41,6 +46,12 @@ var getItemsByStock = function(items,stock) {
 
 //iteration of arrays
 //###############################################################
+//function to use any array and get its title
+//call function (function that output arrays)
+//create title storage array
+//iterate over database to find title for number values within the array
+//push title into array
+//return title
 var call = function(items, functionX) {
   var titleA = [];
   for(var i = 0; i < functionX.length; i++) {
@@ -50,7 +61,12 @@ var call = function(items, functionX) {
   console.log(titleA.join(" || "));
   return titleA;
 };
-
+//comparison between 2 arrays to check for similar numbers
+//make array to keep comparison
+//function (first array, second array)
+//nested for loop / if array 1 == array 2
+//push similar array values to array
+//return array
 var compare = function(items, compare2, compare1) {
   var comparison = [];
   for (var i = 0; i < compare1.length; i++) {
@@ -67,11 +83,16 @@ var compare = function(items, compare2, compare1) {
 
 //SEARCH
 //###############################################################
+//reprompt function (string entry for error made, callback --the function to be redone again)
 var reprompt = function(rep, callback){
   alert("Error: Your " + rep + " entry was invalid please try again");
   callback();
 };
 
+//output function to put processed info into an alert message
+//iterate over array that is inputed into the function
+//total up (price, number and country of items)
+//alert
 var output = function(items, array){
   var totalPrice = 0;
   var numberOfItems = 0;
@@ -85,6 +106,13 @@ var output = function(items, array){
   }
   alert("The total price is " + totalPrice + ", number of items is " + numberOfItems + ", and the countries of production include" + countryOfItems + ".");
 }
+//search by (author brand stock)function
+//define arrays to be called later based on which value is entered into prompt
+//make storage array for array value of selection option (0,1,2)
+//if array value ===0 reprompt
+//if array value ===1 call makearray function and output function
+//if array value ===2 combine both arrays with combine function before doing above
+//if array value ===3 combine any 2 arrays// then compare the last array with the new combined array, making a single array before doing above
 var searchByF = function(searchByArray){
   var absArray = ["author", "brand", "stock"];
   var absFunction = ["getItemsByAuthor", "getItemsByBrand", "getItemsByStock"];
@@ -115,11 +143,19 @@ var searchByF = function(searchByArray){
     output(items, absFunction);
   }
 };
-
+//search function (not really)
+//make storage object for MAX MIN value
+//prompt (search abs or any combi of them)
+//correct input of string (toLowerCase/ .split.join.split)
+//call searchBy function
+//prompt (max price)
+//prompt (min price)
+//correct input of number (parseInt) - reprompt
+//call max min search by function
 var search = function(){
   var maxMinO = {
-    max: "na",
-    min: "na"
+    max: NaN,
+    min: NaN
   }
   var searchBy = prompt("Search by (brand, author, stock) or any combination of them:");
   searchBy = searchBy.toLowerCase();
@@ -128,7 +164,9 @@ var search = function(){
   var maxPrice = prompt("Search by max price/ enter N to decline");
   var minPrice = prompt("Search by min price/ enter N to decline");
 };
-
+//searchBy max min function (max value , min value)
+//
+//
 
 
 //calls
