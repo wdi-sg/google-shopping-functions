7afecf8e3b275ac0d0d42d97e8b045d5067f16ed
“the574th” <sufyazman@gmail.com>
2018-11-30 08:37:13 +0800

    created functions and run scripn on seperate files

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,17 +1,60 @@
+// 1.) getItems(objectData)
+// input: json object
+// returns: an array of items
+// Create a function called getItems that simply returns the items array from the google product object.
+// Note all other functions (below) use the return of this function as their input.
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+var getItems = function(objectData) {
+  return objectData.items;
 }
 
-/*
- * Define and use your functions here
- */
+// 2.) getItemsByBrand(items, brand)
+// input: an array of items, a string of a brand to filter with
+// returns: an array of items (of a specific brand)
+// Create a function called getItemsByBrand that takes an item array returns a new array of all items of a specified brand.
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+// Test this function by searching for Sony, Canon, Nikon and Panasonic.
+
+var getItemsByBrand = function(items, brand) {
+  var array = [];
+  for (var i = 0; i < items.length; i++) {
+    if(items[i].product.brand === brand) {
+      array.push(items[i]);
+    }
+  }
+  return array;
+}
+
+// 3.) getItemsByAuthor(items, author)
+// input: an array of items, a string of an author to filter with
+// returns: an array of items (of a specific author)
+// Create a function called getItemsByAuthor that takes an item array and returns a new array of all items by a specified author.
+
+// Test this function by searching for Target, CDW, eBay
+
+var getItemsByAuthor = function(items, author) {
+  var array = [];
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].product.author.name === author) {
+      array.push(items[i]);
+    }
+  }
+  return array;
+}
+
+
+// 4.) getAvailableProducts(items)
+// input: an array of items
+// returns: an array of items (that are available)
+// Create function called getAvailableProducts that takes an item array and returns an array containing all of the available products (an available product is one with at least one availability of "inStock" in the inventories array)
+
+var getAvailableProducts = function(items) {
+  var array = [];
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].product.inventories[0].availability === "inStock") {
+      array.push(items[i]);
+    }
+  }
+  return array;
+}
 
