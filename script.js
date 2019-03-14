e950b8d93c3c32067dbe1260cfffb4de6310e04a
ViAyDiNe <neel.augusthy@gmail.com>
2019-03-15 00:15:32 +0800

    Question 3 and fixed global var in Question 1

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ console.log(products);
 //Question 1
 console.log("Question 1");
 
-var getItems = function() {
-  var prodArr = [];
+var getItems = function(objectData) {
+  return objectData
 
-  for (var i = 0; i < products.items.length; i++){
-    var item = products.items[i];
-    prodArr.push(item.product);
-  }
-    return prodArr;
+//   var prodArr = [];
+
+//   for (var i = 0; i < products.items.length; i++){
+//     var item = products.items[i];
+//     prodArr.push(item.product);
+//   }
+//     return prodArr;
 }
 
-console.log(getItems());
+// console.log(getItems());
 
 var items = getItems(products.items);
 
@@ -27,7 +29,7 @@ var getItemsByBrand = function(items,itemBrand) {
   var brandArr = [];
 
   for ( var i = 0; i < items.length; i++){
-    if(items[i].brand == itemBrand) {
+    if(items[i].product.brand.toLowerCase() == itemBrand.toLowerCase()) {
       brandArr.push(items[i]);
     }
   }
@@ -39,8 +41,18 @@ console.log(getItemsByBrand(items,"Sony"));
 //Question 3
 console.log("Question 3");
 
+var getItemsByAuthor = function(items,itemAuthor) {
+  var authorArr = [];
 
+  for ( var i = 0; i < items.length; i++){
+    if(items[i].product.author.name.includes(itemAuthor)) {
+      authorArr.push(items[i]);
+    }
+  }
+  return authorArr;
+}
 
+console.log(getItemsByAuthor(items,"eBay"));
 
 
 
