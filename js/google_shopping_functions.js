285b2957cc4d65e48f8c5994d739ae132807bacf
seeo <aragonlotr5@gmail.com>
2019-03-14 22:05:23 +0800

    first commit

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -4,9 +4,9 @@ console.log("...functions.js is up and running!");
  * input: accepts full item data
  * output: returns the length of the items array
  */
-function getItemsCount(itemData) {
-  return itemData.items.length;
-}
+//function getItemsCount(itemData) {
+//  return itemData.items.length;
+//}
 
 /*
  * Define and use your functions here
@@ -26,14 +26,14 @@ var getItemsByBrand = function(array,brandName){
   //console.log("is the for loop happening?");
     if ((array[i].product.brand) == brandName){
     // console.log("Is this push into the array happening?");
-      brandArr.push(array[i].product.title);
+      brandArr.push(array[i]);
     }else{
       //do nothing
     }
   }
   console.log("prints out the Brand Array");
   console.log(brandArr);
-  //return brandArr;
+  return brandArr;
 }
 
 //Part 3:
@@ -41,13 +41,14 @@ var getItemsByAuthor = function(array, authorName){
   var authorArr = [];
   for (i = 0; i < array.length; i++){
     if((array[i].product.author.name.includes(authorName))){
-      authorArr.push(array[i].product.title);
+      authorArr.push(array[i]);
     }else{
       //do nothing;
     }
   }
   console.log("prints out array of items with an author that matches the user's input");
   console.log(authorArr);
+  return authorArr;
 }
 
 //Part 4:
@@ -55,17 +56,32 @@ var getAvailableProducts = function(array){
   var inStockArr = [];
   for (i = 0; i< array.length; i++){
     if((array[i].product.inventories[0].availability) === "inStock"){
-      inStockArr.push(array[i].product.title);
+      inStockArr.push(array[i]);
     }else{
       //do nothing;
     }
   }
-  console.log("prints out array of items that are in stock");
+  console.log("prints out array of all items that are in stock");
   console.log(inStockArr);
+  return inStockArr;
 }
 
-//Part 5:
+//Part 5, all items made by Sony:
+console.log("Create array that outputs all items made by Sony");
+var getSonyItems = getItemsByBrand(getItems(),"Sony");
 
+
+//Part 5, All items made by Sony that are available:
+console.log("Create array that outputs all items made by Sony that are in stock");
+var getSonyItemsInStock = getItemsByBrand(getAvailableProducts(getItems()),"Sony");
+
+//Part 5, All items with author name "Adorama Camera" that are available:
+console.log("Create aray that outputs all items Adorama Camera that are in stock");
+var getAdoramaItemsInStock = getItemsByAuthor(getAvailableProducts(getItems()),"Adorama Camera");
+
+//Part 5, All items with author name "eBay", brand name of Nikon;
+console.log("Create array that outputs all items author name of eBay and brand name of Nikon");
+var getItemsNikonFromEbay = getItemsByAuthor(getItemsByBrand(getItems(),"Nikon"),"eBay");
 
 
 
--- script.js
+++ script.js
@@ -5,23 +5,58 @@ console.log("Part 1: Get Items is done");
 
 
 //Part 2:
-console.log("Executing Get Items by brands from the script page");
+console.log("Calling Get Items by brands from the script page");
 getItemsByBrand(getItems(),"Sony");
 getItemsByBrand(getItems(),"Canon");
 getItemsByBrand(getItems(),"Nikon");
 getItemsByBrand(getItems(),"Panasonic");
-console.log("End executing get items by brands from the script page");
+console.log("End call get items by brands from the script page");
 
 
 //Part 3: Printing out an array of items that include these words in the author name: Target, CDW, eBay;
+console.log("Calling all items with author name Target");
 getItemsByAuthor(getItems(),"Target");
+console.log("Calling all items with author name CDW");
 getItemsByAuthor(getItems(),"CDW");
+console.log("Calling all items with author name eBay");
 getItemsByAuthor(getItems(),"eBay");
 
 //Part 4: Printing out an array of items that are in stock
+console.log("Calling all items that are in stock");
 getAvailableProducts(getItems());
 
-//Part 5: 
+//Part 5, All itesms made by Sony:
+console.log("Calling all items made by Sony");
+console.log(getSonyItems);
 
+//Part 5, All items made by Sony that are available:
+console.log("Calling all items made by Sony that are in stock");
+console.log(getSonyItemsInStock);
 
-console.log('end');
+//Part 5, All items with author name "Adorama Camera" that are available:
+console.log("Calling all items with author name Adorama and are in stock");
+console.log(getAdoramaItemsInStock);
+
+//Part 5, All items with author name "eBay", brand name of Nikon;
+console.log("Calling all items with author name eBay and made by Nikon");
+console.log(getItemsNikonFromEbay);
+
+console.log('end of basic requirements');
+
+console.log('Begin further requirements here');
+//Further part 1: Prompt the user for the search term they are looking for.
+var searchTerm = prompt("Hi! What are you searching for? 'brand', 'author' or 'availability'?");
+if (searchTerm == "brand"){
+    searchBrand = prompt("What brand are you looking for?");
+    if(searchBrand == "Sony" || "Lytro" || "Nikon" || "Canon" ||"Panasonic" ||"Samsung"){
+      getItemsByBrand(getItems(),searchBrand);
+    }else{
+      console.log("Sorry, we do not carry this brand.");
+    }
+}else if(searchTerm == "author"){
+
+}else if(searchTerm == "availability"){
+
+}
+  }
+}
