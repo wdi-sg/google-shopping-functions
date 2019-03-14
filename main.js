6915403f78209213eb04e91a716677aad108a16d
moses <moseslee90@gmail.com>
2019-03-14 17:15:43 +0800

    part 3

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-var getItems = function (objectData) {
+var getItems = function(objectData) {
     return objectData.items;
     //items is an array of item Objects
-}
+};
+items = getItems(products);
+
 function getItemsByBrand(items, brandName) {
-    //array which will hold our brand search results
-    var brandArray = [];
-    for (let i = 0; i < items.length; i++) {
-        //brand reseult found in lowercase
-        var brandResult = items[i].product.brand.toLowerCase();
-        if (brandResult === brandName.toLowerCase()) {
-            brandArray.push(items[i]);
-        }
+  //array which will hold our brand search results
+  var brandArray = [];
+  for (let i = 0; i < items.length; i++) {
+    //brand reseult found in lowercase
+    var brandResult = items[i].product.brand.toLowerCase();
+    if (brandResult.includes(brandName.toLowerCase())) {
+      brandArray.push(items[i]);
     }
-    return brandArray;
+  }
+  return brandArray;
 }
-getItems(products);
+
+function getItemsByAuthor(items, authorName) {
+  var authorArray = [];
+  for (let i = 0; i < items.length; i++) {
+    //author reseult found in lowercase
+    var authorResult = items[i].product.author.name.toLowerCase();
+    if (authorResult.includes(authorName.toLowerCase())) {
+      authorArray.push(items[i]);
+    }
+  }
+  return authorArray;
+}
