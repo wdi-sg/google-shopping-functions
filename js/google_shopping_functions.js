71583b2c0b670c67dabfc965759d753713da225b
GeekHarp <ngbhandy@gmail.com>
2019-03-14 23:33:55 +0800

    Assignment done

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,17 +1,64 @@
+const arrOfItems = products.items;
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
 function getItemsCount(itemData) {
   return itemData.items.length;
 }
+// console.log('Item Count: ' + getItemsCount(products));
 
-/*
- * Define and use your functions here
- */
+ function getItems(objectData){
+  return objectData.items;
+ }
+// console.log('Array: ' + getItems(products));
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+ function getItemsByBrand(items,brand){
+  let arr = [];
+  for (let i = 0; i < items.length; i++) {
+    let itemBrand = items[i].product.brand.toLowerCase();
+    if (itemBrand.includes(brand.toLowerCase()) ) {
+      arr.push(items[i]);
+    }
+  }
+  return arr;
+ }
+ // console.log(getItemsByBrand(arrOfItems,"Canon"))
+
+function getItemsByAuthor(items,author){
+  let arr = [];
+  for (let i = 0; i < items.length; i++) {
+    let itemAuthor = items[i].product.author.name.toLowerCase();
+    if (itemAuthor.includes(author.toLowerCase())) {
+      arr.push(items[i]);
+    }
+  }
+  return arr;
+}
+// console.log(getItemsByAuthor(arrOfItems,"eBay"));
+
+function getAvailableProducts(items){
+  let arr = [];
+  for (let i = 0; i < items.length; i++) {
+    let available = items[i].product.inventories[0].availability;
+    if (available === "inStock" ) {
+      arr.push(items[i]);
+    }
+  }
+  return arr;
+}
+// console.log(getAvailableProducts(arrOfItems));
+
+//Part 5
+const sonyProducts = getItemsByBrand(arrOfItems,"Sony");
+// console.log(sonyProducts);
+
+const availableSonyProducts = getAvailableProducts(sonyProducts);
+// console.log(availableSonyProducts);
+
+const adoramaCam = getItemsByAuthor(arrOfItems,"Adorama Camera");
+// console.log(adoramaCam);
+
+const nikonProducts = getItemsByBrand(arrOfItems,"Nikon");
+const nikonByEbay   = getItemsByAuthor(nikonProducts,"eBay");
+console.log(nikonByEbay);
+
+
 
