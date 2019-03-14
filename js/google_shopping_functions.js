47ddccb88bb9b3985e32adf15cd8c83f1322d65a
seeo <aragonlotr5@gmail.com>
2019-03-14 19:09:35 +0800

    done with questions 1 and 2

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,4 +1,4 @@
-
+console.log("...functions.js is up and running!");
 /*
  * example function called getItemsCount
  * input: accepts full item data
@@ -11,6 +11,74 @@ function getItemsCount(itemData) {
 /*
  * Define and use your functions here
  */
+//Part 1: items array
+var getItems = function(){
+  console.log(products.items);
+  return(products.items);
+}
+/*
+//Part 2: get items by brand
+var getItemsByBrand = function(brandName){
+  var brandArr = [];
+  //console.log("Just before the loop happens");
+  //define the getItems function as a variable just so that it is easier to call and use;
+  var getI = getItems();
+  for (i in getI){
+  //  console.log("is the for loop happening?");
+    if ((getI[i].product.brand) == brandName){
+    //  console.log("Is this push into the array happening?");
+      brandArr.push(getI[i].product.title);
+    }else{
+      //do nothing;
+    }
+  }
+  //console.log("prints out the Brand Array");
+  console.log(brandArr);
+  //return brandArr;
+}
+//console.log("execute get item by sony brand");
+*/
+//getItemsByBrand("Sony");
+//Part 2.1: Here i put both brandName and the array from earlier which has function name of getItems:
+var getItemsByBrand = function(array,brandName){
+  var brandArr = [];
+  //console.log("Just before the loop happens");
+  //define the getItems function as a variable just so that it is easier to call and use;
+  for (i = 0; i < array.length; i++){
+  //console.log("is the for loop happening?");
+    if ((array[i].product.brand) == brandName){
+    // console.log("Is this push into the array happening?");
+      brandArr.push(array[i].product.title);
+    }else{
+      //do nothing
+    }
+  }
+  console.log("prints out the Brand Array");
+  console.log(brandArr);
+  //return brandArr;
+}
+//getItemsByBrand(getItems(),"Sony");
+
+
+//console.log("Test console log for index 0 of the items array");
+//console.log(products.items[0]);
+//console.log(getItems[0]);
+console.log("end");
+
+//Part 3:
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+//console.log('Item Count: ' + getItemsCount(data));
---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Script.js is up and running!");
+
+getItems();
+console.log("Part 1: Get Items is done");
+
+
+//Part 2:
+console.log("Executing Get Items by brands from the script page");
+getItemsByBrand(getItems(),"Sony");
+getItemsByBrand(getItems(),"Canon");
+
+console.log("End executing get items by brands from the script page");
+
+//Canon, Nikon and Panasonic.
+
+
+//Part 3:
+
+
+
+console.log('end');
