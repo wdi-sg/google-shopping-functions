3cff69a0b496a4f10cc62bf9d1582574b6185fbf
valenlyn <valenlynchua@gmail.com>
2019-03-14 22:48:23 +0800

    Progress until part 5.3.

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -5,10 +5,9 @@
  * output: returns the length of the items array
  */
 
-
-function getItemsCount(itemData) {
-  return itemData.length;
-}
+// function getItemsCount(itemData) {
+//   return itemData.length;
+// }
 
 /*
  * Define and use your functions here
@@ -21,26 +20,65 @@ function getItems(objectData) {
 
 var brandItems = [];
 
-function getItemsByBrand(items, brand) {
+function getItemsByBrand(items,brand) {
   for (var i = 0; i < items.length; i++) {
-    var productBrand = products["items"][i]["product"]["brand"].toLowerCase();
+    var productBrand = products["items"][i]["product"]["brand"];
 
-    if (brand.toLowerCase() === productBrand) {
+    if (brand === productBrand) {
       brandItems.push(products["items"][i]["product"]["title"]);
     }
   }
   return brandItems;
 }
 
-// var brandItems = [];
+/* 3. Get a list of products by specifying author's name. */
 
-// function getItemsByAuthor(items, author) {
-//   for (var i = 0; i < items.length; i++) {
-//     var authorName = products["items"][i]["product"]["brand"].toLowerCase();
-//     if (author.toLowerCase() === authorName) {
-//       brandItems.push(products["items"][i]["product"]["title"];
+var itemsAuthor = [];
+
+function getItemsByAuthor(items,author) {
+  // console.log(author)
+  for (var i = 0; i < items.length; i++) {
+    var authorName = products["items"][i]["product"]["author"]["name"].toLowerCase();
+    author = author.toLowerCase();
+    if (authorName.includes(author) || authorName === author) {
+      itemsAuthor.push(products["items"][i]["product"]["title"]);
+    }
+  }
+  return itemsAuthor;
+}
+
+// console.log(itemsAuthor);
+
+/* 4. Get a list of products by specifying author's name. */
+
+var availableProducts = [];
+var availProductsDetails = [];
+
+function getAvailableProducts(items) {
+
+  for (var i = 0; i < items.length; i++) {
+    if (products["items"][i]["product"]["inventories"][0]["availability"] === "inStock") {
+    availableProducts.push(products["items"][i]["product"]["title"]);
+    availProductsDetails.push(products["items"][i]);
+    }
+  }
+  return availableProducts;
+}
+
+/* 5. Use your functions. */
+
+// var result = [];
+
+// function compare(array1,array2) {
+//   for (var i = 0; i < array1.length; i++) {
+//     for(var j = 0; j < array2.length; j++ ) {
+//       if(array1[i] === array2[j]) {
+//         result.push(products["items"][i]["product"]["title"]);
+//       }
 //     }
-
+//   }
+//   return result;
+// }
 
 
 // output item count using the getItemsCount function
--- script.js
+++ script.js
@@ -1,7 +1,61 @@
-console.log("hi");
+// console.log(getItems());
 
-console.log(getItems());
+// console.log(getItemsByBrand(getItems(),"canon"));
 
-console.log(getItemsByBrand(getItems(), "Canon"));
+// console.log(getItemsByAuthor(getItems(),"Overstockdigital"));
 
-getItemsByAuthor(getItems(), "author");
+// console.log(getAvailableProducts(getItems()));
+
+/* Use your functions */
+
+// // 1. All items made by Sony.
+var sony = getItemsByBrand(getItems(),"Sony");
+console.log(sony);
+
+// 2. All items made by Sony that are available.
+
+var sonyAvailable = [];
+getAvailableProducts(getItems());
+
+for (var i = 0; i < availableProducts.length; i++) {
+  if (availProductsDetails[i]["product"]["brand"] === "Sony") {
+    sonyAvailable.push(availProductsDetails[i]["product"]["title"]);
+    console.log("This item is made by Sony, and is available: " + availProductsDetails[i]["product"]["title"]);
+  }
+}
+
+console.log("Summary: " + sonyAvailable);
+
+// 3. All available items by the author "Adorama Camera"
+
+var adoramaAvailable = [];
+var adorama = getItemsByAuthor(getItems(),"Adorama Camera");
+
+for (var i = 0; i < availableProducts.length; i++) {
+  if (availProductsDetails[i]["product"]["author"]["name"] === "Adorama Camera") {
+    adoramaAvailable.push(availProductsDetails[i]["product"]["title"]);
+    console.log("This item is by Adorama Camera, and is available: " + availProductsDetails[i]["product"]["title"]);
+  }
+}
+
+console.log(adoramaAvailable);
+
+// 4. All items made by Nikon with the author eBay.
+
+// var nikonEbay = [];
+// var nikon = getItemsByBrand(getItems(),"nikon");
+// var ebay = getItemsByAuthor(getItems(),"ebay");
+
+// compare(nikon,ebay);
+// console.log(result + "hi");
+
+
+// for (var i = 0; i < brandItems2.length; i++) {
+//   for(var j = 0; j < available.length; j++ ) {
+//     if(brandItems2[i] === available[j]) {
+//       adoramaAvailable.push(products["items"][i]["product"]["title"]);
+//     }
+//   }
+// }
+
+
