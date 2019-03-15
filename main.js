9eae8fc5db1f4159e01e32f5dbb023d615d03122
moses <moseslee90@gmail.com>
2019-03-15 15:59:16 +0800

    typo error fixed

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ function maximumAndMinimum() {
 
 function userPrompt() {
   var userSearchTerm = prompt(
-    "Enter the number for action to execute: 1 for number of products 2 for country of items, 3 for Total price of inventory, 4 to search for brands, 5 to search through authors 6 to search via max or min price "
+    "Enter the number for action to execute: 1 for number of products 2 for country of items, 3 for Total price of inventory, 4 to search for brands, 5 to search through authors 6 to search via max or min price 7 to set max and min"
   );
   switch (userSearchTerm) {
     case "1":
