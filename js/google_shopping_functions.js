5949f76ebbd88449803a0738e4384a03b45b0a1e
reynosoturtle <christianreynoso96@gmail.com>
2019-03-15 09:07:37 +0800

    first commit

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -1,17 +1,52 @@
 
-/*
- * example function called getItemsCount
- * input: accepts full item data
- * output: returns the length of the items array
- */
-function getItemsCount(itemData) {
-  return itemData.items.length;
+// /*
+//  * example function called getItemsCount
+//  * input: accepts full item data
+//  * output: returns the length of the items array
+//  */
+// function getItemsCount(itemData) {
+//   return itemData.items.length;
+// }
+
+// /*
+//  * Define and use your functions here
+//  */
+
+// // output item count using the getItemsCount function
+// console.log('Item Count: ' + getItemsCount(data));
+
+var getItems = function(objectData) {
+  return objectData;
 }
 
-/*
- * Define and use your functions here
- */
+var items = getItems(products.items);
 
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+var getItemsByBrand = function(items, itemBrand) {
+  var pushArray = [];
+  for (i=0; i<items.length; i++) {
+    if (items[i].product.brand === itemBrand) {
+      pushArray.push(items[i]);
+    }
+  }
+  return pushArray;
+}
 
+var getItemsByAuthor =  function(items, itemAuthor) {
+  var pushArray = [];
+  for (i=0; i<items.length; i++) {
+    if (items[i].product.author.name.includes(itemAuthor)) {
+      pushArray.push(items[i]);
+    }
+  }
+  return pushArray;
+}
+
+var getAvailableProducts = function(items) {
+  var pushArray = [];
+  for (i=0; i<items.length; i++) {
+    if (items[i].product.inventories[0].availability === 'inStock') {
+      pushArray.push(items[i]);
+    }
+  }
+  return pushArray;
+}
